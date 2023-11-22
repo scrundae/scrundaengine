5ceed1efb7b9c70926193cb5b1e1443ccfd99762
Harry Collins <sensorydog@gmail.com>
2023-11-22 21:26:25 +1000

    Add project files.

---
+++ scrundaengine/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace scrundaengine
+{
+    public class Engine
+    {
+        //Health variables
+        public float globalHealth = 100;
+        public float minGlobalHealth = 0;
+        public float maxGlobalHealth = 100;
+        public bool useGlobalHealth;
+        public string globalUserScrape;
+        public string globalUserRecord;
+        public void Cast(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void Wipe()
+        {
+            Console.Clear();
+        }
+
+        public void Scrape()
+        {
+            globalUserScrape = Console.ReadLine();
+        }
+
+        public void Record()
+        {
+            globalUserRecord = Console.ReadKey().KeyChar.ToString();
+        }
+
+        public void PlotPoint(float x, float y, string pointDefiner)
+        {
+            string formattedPoint = new string(' ', (int)x) + pointDefiner; // Add spaces before 'o' based on the value of x
+
+            for (int i = 0; i < (int)y; i++)
+            {
+                Console.WriteLine(); // Add new lines before 'o' based on the value of y
+            }
+
+            Console.WriteLine(formattedPoint);
+        }
+    }
+}
